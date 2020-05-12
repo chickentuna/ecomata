d4a6d796997346646d2af1ca7c27b27d8e759041
Julien Poulton <julien@codingame.com>
2020-05-12 20:16:02 +0200

    create react app

---
+++ src/react/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.scss'
+
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-header-title'>Ecomata</h1>
+      </header>
+      <div className='App-content' />
+    </div>
+  )
+}
+
+export default App
