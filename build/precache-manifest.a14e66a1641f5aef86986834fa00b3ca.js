45a3bc64e19063808a90d7f8a5fa406844e17339
GitHub Action <action@github.com>
2020-09-05 01:15:02 +0000

    rebuild

---
+++ build/precache-manifest.a14e66a1641f5aef86986834fa00b3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d77b6e4c0e0831099dfbc4b43d5024",
+    "url": "/ecomata/build/index.html"
+  },
+  {
+    "revision": "77f66de997f61a31b927",
+    "url": "/ecomata/build/static/css/main.66de3ae3.chunk.css"
+  },
+  {
+    "revision": "c2d053506ca393d85fdf",
+    "url": "/ecomata/build/static/js/2.438c5f9d.chunk.js"
+  },
+  {
+    "revision": "13f62c8e7923c0635904f3f7bab955af",
+    "url": "/ecomata/build/static/js/2.438c5f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f66de997f61a31b927",
+    "url": "/ecomata/build/static/js/main.7f7e5d0e.chunk.js"
+  },
+  {
+    "revision": "aa91aec8ceb5573b5cc6",
+    "url": "/ecomata/build/static/js/runtime-main.469de872.js"
+  }
+]);
